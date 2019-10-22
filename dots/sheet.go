ca0a8a4978b7c23f6de6feabe22589719820b7ae
Mercanuis <brogramn@gmail.com>
2019-10-22 19:39:16 +0900

    Attempts to fix solution for bleeding as grid does not produce correct solution consistently

--- dots/sheet.go
+++ dots/sheet.go
@@ -17,16 +17,18 @@ func NewSheet(rows, cols int, dots []Dot) Sheet {
 }
 
 func (s *Sheet) SolveSheet() int {
+	fmt.Println("STARTING SHEET")
+
 	grid := make([][]int, s.NumRows)
 	for i := range grid {
 		grid[i] = make([]int, s.NumCols)
 	}
 
-	for _, dot := range s.Dots {
-		s.doBleedDarkness(dot.Row, dot.Col, dot.Darkness, grid)
-		fmt.Println("FINISHED DOT")
-	}
-	fmt.Println("DONE WITH DOTS")
+	//for _, dot := range s.Dots {
+	s.doBleedDarkness(grid)
+	//	fmt.Println("FINISHED DOT")
+	//}
+	//fmt.Println("DONE WITH DOTS")
 
 	total := 0
 	for i := range grid {
@@ -38,7 +40,7 @@ func (s *Sheet) SolveSheet() int {
 	return total
 }
 
-func (s *Sheet) doBleedDarkness(row int, col int, dark int, grid [][]int) {
+func (s *Sheet) doBleedDarkness(grid [][]int) {
 	//reset the memo for each dot
 	memo := make([][]bool, s.NumRows)
 	for i := range memo {
@@ -51,37 +53,49 @@ func (s *Sheet) doBleedDarkness(row int, col int, dark int, grid [][]int) {
 		}
 	}
 
-	s.bleedDarkness(row, col, dark, grid, memo)
+	for _, dot := range s.Dots {
+		s.bleedDarkness(dot.Row, dot.Col, dot.Darkness, grid, memo)
+		fmt.Println("FINISHED DOT")
+	}
+	fmt.Println("DONE WITH DOTS")
+
+	//s.bleedDarkness(row, col, dark, grid, memo)
 }
 
 func (s *Sheet) bleedDarkness(row int, col int, dark int, grid [][]int, memo [][]bool) {
+	fmt.Printf("row = %d, col = %d, dark = %d\n", row, col, dark)
+
 	if row < 0 || row >= s.NumRows || col < 0 || col >= s.NumCols {
+		fmt.Printf("OUT OF BOUNDS! Dark is at [%d]\n", dark)
 		//Out of bounds
 		return
 	} else if memo[row][col] {
 		//We've seen this before, but its possible another path could lead to a higher dark value
 		//Make a check and then return
 		if dark > grid[row][col] {
+			fmt.Printf("UPDATE VISITED CELL: [row %d, col %d] dark is now [%d]\n", row, col, dark)
 			grid[row][col] = dark
 		}
 		return
 	} else if grid[row][col] > dark {
 		//Current value > dark, return
+		fmt.Printf("[row %d, col %d] dark [%d] > [%d]\n", row, col, grid[row][col], dark)
 		memo[row][col] = true
 		return
 	} else {
 		//This is a cell to update, update the cell, memo, and then traverse
+		fmt.Printf("UPDATE CELL: [row %d, col %d] dark is now [%d]\n", row, col, dark)
 		grid[row][col] = dark
-		dark--
+		//dark--
 		memo[row][col] = true
 
 		//left
-		s.bleedDarkness(row-1, col, dark, grid, memo)
+		s.bleedDarkness(row-1, col, dark-1, grid, memo)
 		//right
-		s.bleedDarkness(row+1, col, dark, grid, memo)
+		s.bleedDarkness(row+1, col, dark-1, grid, memo)
 		//up
-		s.bleedDarkness(row, col-1, dark, grid, memo)
+		s.bleedDarkness(row, col-1, dark-1, grid, memo)
 		//down
-		s.bleedDarkness(row, col+1, dark, grid, memo)
+		s.bleedDarkness(row, col+1, dark-1, grid, memo)
 	}
 }
--- main/main.go
+++ main/main.go
@@ -9,31 +9,12 @@ func main() {
 	totalRows := 3
 	totalCols := 3
 
-	dot1 := dots2.NewDot(0, 0, 10)
-	dot2 := dots2.NewDot(1, 2, 10)
+	dot1 := dots2.NewDot(0, 0, 15)
+	dot2 := dots2.NewDot(2, 2, 15)
 
 	dots := []dots2.Dot{dot1, dot2}
 
 	s := dots2.NewSheet(totalRows, totalCols, dots)
 	fmt.Println(s.SolveSheet())
 	fmt.Println()
-
-	totalRows = 5
-	totalCols = 5
-
-	/*
-		   0   1   2   3   4
-		0
-		1         25
-		2
-		3
-		4
-	*/
-
-	dot3 := dots2.NewDot(1, 2, 25)
-	dot4 := dots2.NewDot(3, 4, 25)
-
-	dots22 := []dots2.Dot{dot3, dot4}
-	s2 := dots2.NewSheet(totalRows, totalCols, dots22)
-	fmt.Println(s2.SolveSheet())
 }
